a3a95c6d1e2d591d8792fc1893e7fd618bc39edd
Felix Indrawan <indrawan.felix123@gmail.com>
2024-03-11 21:46:56 -0400

    fix: Added connectionstr and ssl

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,11 @@ const pool = new Pool({
   port: process.env.DB_PORT_NUMBER,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectionString: `${process.env.DATABASE_URL}?sslmode=require`, // Use the DATABASE_URL from your Heroku config vars
+  ssl: {
+    rejectUnauthorized: true // This should only be used for development
+  }
 })
 
 module.exports = pool
